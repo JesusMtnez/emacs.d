76c35650834e47671fc39408e6791f4f0097c324
JesusMtnez <jesusmartinez93@gmail.com>
2015-09-12 19:48:29 +0200

    [EMACS] Remove marmalade and melpa-stable repos.

--- init.el
+++ init.el
@@ -4,8 +4,6 @@
 
 ;; MELPA and Marmalade repos for packages.
 (require 'package)
-;;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
