0086d05d7e37bca6548504f0a6b27f176f211fd7
JesusMtnez <jesusmartinez93@gmail.com>
2015-11-25 18:38:41 +0100

    [EMACS] Add git-gutter-fringe+ in UI

--- init.el
+++ init.el
@@ -55,6 +55,23 @@
   :ensure t
   :defer t)
 
+;; git-gutter-fringe+
+(use-package git-gutter-fringe+
+  :ensure t
+  :if window-system ; Only when window-system
+  :init (progn (global-git-gutter+-mode))
+  :config (progn (define-key git-gutter+-mode-map (kbd "C-x n") 'git-gutter+-next-hunk)
+                 (define-key git-gutter+-mode-map (kbd "C-x p") 'git-gutter+-previous-hunk)
+                 (define-key git-gutter+-mode-map (kbd "C-x v =") 'git-gutter+-show-hunk)
+                 (define-key git-gutter+-mode-map (kbd "C-x r") 'git-gutter+-revert-hunks)
+                 (define-key git-gutter+-mode-map (kbd "C-x t") 'git-gutter+-stage-hunks)
+                 (define-key git-gutter+-mode-map (kbd "C-x c") 'git-gutter+-commit)
+                 (define-key git-gutter+-mode-map (kbd "C-x C") 'git-gutter+-stage-and-define)
+                 (define-key git-gutter+-mode-map (kbd "C-x C-y") 'git-gutter+-stage-and-commit-whole-buffer)
+                 (define-key git-gutter+-mode-map (kbd "C-x U") 'git-gutter+-unstage-whole-buffer))
+  :diminish (git-gutter+-mode . "gg")
+)
+
 ;; Helm
 (use-package helm
   :ensure t
