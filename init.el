1fe03e9a133f623e02770064a69cfb381b58d1d9
JesusMtnez <jesusmartinez93@gmail.com>
2015-12-16 18:42:23 +0100

    [EMACS] Fix issue for emacs-mac
    When emacs is running with window system, and I change to another
    desktop, when returning to the desktop where emacs is running, it loses
    focus.
    
    (╯°□°）╯︵ ┻━┻
    
    If the menu-bar is active, this issue is solve.
    More info: https://github.com/railwaycat/emacs-mac-port/issues/79

--- init.el
+++ init.el
@@ -277,6 +277,8 @@
     (progn (setq mac-command-modifier 'meta) ; left-cmd-key as meta-key
            (setq mac-option-modifier 'meta) ; left-option-key as meta-key
            (setq mac-right-option-modifier nil) ; right-option-key as option-key
+           (when window-system
+             (menu-bar-mode t)))) ; Restore menu-bar to fix: https://github.com/railwaycat/emacs-mac-port/issues/79
 ;; (setq inhibit-startup-screen t)
 
 ;; Key bindings
