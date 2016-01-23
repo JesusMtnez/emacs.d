d968b8fc1df5fe46d42fc874d3e9f35a7747ef72
JesusMtnez <jesusmartinez93@gmail.com>
2016-01-23 10:46:54 +0100

    [EMACS] Move customizations to external file

--- init.el
+++ init.el
@@ -282,33 +282,9 @@
 (global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region-or-line)
 (global-set-key (kbd "C-x C-k") 'kill-this-buffer)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(livedown:autostart t)
- '(livedown:open t)
- '(livedown:port 1337)
- '(sh-basic-offset 2)
- '(sh-indentation 2))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(neo-dir-link-face ((t (:foreground "DeepSkyBlue"))))
- '(neo-expand-btn-face ((t (:foreground "SkyBlue"))))
- '(neo-file-link-face ((t (:foreground "white"))))
- '(neo-header-face ((t (:foreground "LightBlue" :weight bold))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "yellow"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
+;; Customizations
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
 
 (if (file-exists-p "~/.emacs.d/localrc.el")
     (load "~/.emacs.d/localrc.el"))
