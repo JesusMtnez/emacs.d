c1cfb0790a7c5ee3927bad750adcf21a738e6063
JesusMtnez <jesusmartinez93@gmail.com>
2016-01-27 20:41:43 +0100

    [EMACS] Add function in Scala IDE configuration
    The function 'scala/enable-eldoc' show the type of the symbol under the
    cursor using ENSIME and ElDoc.

--- scala-conf.el
+++ scala-conf.el
@@ -1,7 +1,16 @@
 ;; Configuration for Scala
 
+(defun scala/enable-eldoc ()
+  "Show the type of the symbol under the cursor"
+  (setq-local eldoc-documentation-function
+              (lambda ()
+                (when (ensime-connected-p)
+                  (ensime-print-type-at-point))))
+  (eldoc-mode +1))
+
 ;; ENSIME
 (use-package ensime
   :ensure t
   :defer t
-  :config (progn (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)))
+  :config (progn (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+                 (add-hook 'ensime-mode-hook #'scala/enable-eldoc)))
