c92ee9ed0e011ca88199a6a760ed33b1e46e976d
JesusMtnez <jesusmartinez93@gmail.com>
2016-01-27 20:53:29 +0100

    [EMACS] Improve ENSIME autocompletion
    The function 'scala/completion-dot' show company completions after
    inserting a '.'

--- scala-conf.el
+++ scala-conf.el
@@ -8,9 +8,19 @@
                   (ensime-print-type-at-point))))
   (eldoc-mode +1))
 
+(defun scala/completing-dot ()
+  "Insert a period and show company completions."
+  (interactive "*")
+  (when (s-matches? (rx (+ (not space)))
+                    (buffer-substring (line-beginning-position) (point)))
+    (delete-horizontal-space t))
+  (insert ".")
+  (company-complete))
+
 ;; ENSIME
 (use-package ensime
   :ensure t
   :defer t
+  :bind (("." . scala/completing-dot))
   :config (progn (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
                  (add-hook 'ensime-mode-hook #'scala/enable-eldoc)))
