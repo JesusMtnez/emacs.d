474229fc5499d58c04ba6b563f0b70d8ee43944f
JesusMtnez <jesusmartinez93@gmail.com>
2016-02-21 12:56:06 +0100

    [EMACS] Add path setup in MacOSX

--- macosx-conf.el
+++ macosx-conf.el
@@ -1,7 +1,15 @@
 ;; Mac OSX special configurations
 
-(setq mac-command-modifier 'meta) ; left-cmd-key as meta-key
-(setq mac-option-modifier 'meta) ; left-option-key as meta-key
-(setq mac-right-option-modifier nil) ; right-option-key as option-key
+; left-cmd-key as meta-key
+(setq mac-command-modifier 'meta)
+; left-option-key as meta-key
+(setq mac-option-modifier 'meta)
+; right-option-key as option-key
+(setq mac-right-option-modifier nil)
+
+; GUI only settings
 (when window-system
-  (menu-bar-mode t)) ; Restore menu-bar to fix: https://github.com/railwaycat/emacs-mac-port/issues/79
+  (menu-bar-mode t) ; Restore menu-bar to fix: https://github.com/railwaycat/emacs-mac-port/issues/79
+  (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin")) ; Add brew path in PATH
+  (setq exec-path (append exec-path '("/usr/local/bin"))) ; Add brew bin in ExecPath
+)
