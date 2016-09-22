f310cbcbf34f83b9741d58bde135a57244fb02db
JesusMtnez <jesusmartinez93@gmail.com>
2016-09-22 16:22:37 +0200

    [EMACS] Remove tern from company-backends

--- init.el
+++ init.el
@@ -50,8 +50,7 @@
   :config (progn (add-hook 'after-init-hook 'global-company-mode)
                  (setq company-idle-delay 0.1)          ; Reduce company auto complete start time
                  (setq company-minimum-prefix-length 1) ; Reduce prefix to start completion
-                 (setq company-show-numbers t)          ; Show number to quick access with M-number
-                 (add-to-list 'company-backends 'company-tern))
+                 (setq company-show-numbers t))         ; Show number to quick access with M-number
   :diminish (company-mode . "CM"))
 ;; TODO: Company integration with helm: https://github.com/yasuyk/helm-company
 
