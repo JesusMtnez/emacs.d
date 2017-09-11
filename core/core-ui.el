62ec3245a6702b4f6058625a9d337a37ccb204f9
JesusMtnez <jesusmartinez93@gmail.com>
2017-09-11 16:25:09 +0200

    Add dashboard to core-ui

--- core/core-ui.el
+++ core/core-ui.el
@@ -11,6 +11,16 @@
 ;; - atom-one-dark-theme: https://github.com/jonathanchu/atom-one-dark-theme/
 ;; - atom-dark-theme-emacs:  https://github.com/whitlockjc/atom-dark-theme-emacs
 
+(use-package dashboard
+  ; repo: https://github.com/rakanalh/emacs-dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-items '((projects  . 5)
+                        (recents . 5))
+        dashboard-startup-banner 'logo
+        dashboard-banner-logo-title "The One True Editor, Emacs"))
+
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'menu-bar-mode)   (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode)   (tool-bar-mode -1))
