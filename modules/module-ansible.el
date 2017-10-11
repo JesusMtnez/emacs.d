712e8ef078523a8fbe3538047d6136d6d602628b
JesusMtnez <jesusmartinez93@gmail.com>
2017-10-11 10:58:14 +0200

    Remove ansible support
    The original package from Spotify has been removed, so I have remove
    this module until I find a new replacement.

--- modules/module-ansible.el
+++
@@ -1,9 +0,0 @@
-;;; module-ansible.el --- ansible integration
-
-(use-package ansible
-  ; repo: https://github.com/spotify/ansiblefile-mode
-  :mode (("\\.ansiblefile" . ansiblefile-mode)
-         ("Ansiblefile\\'" . ansiblefile-mode)))
-
-(provide 'module-ansible)
-;;; module-ansible.el ends here
