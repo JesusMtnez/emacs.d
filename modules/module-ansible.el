b52ba021d202dd89260890b53ed19a2538d064d6
JesusMtnez <jesusmartinez93@gmail.com>
2017-10-11 10:11:20 +0200

    Fix Ansible package name

--- modules/module-ansible.el
+++ modules/module-ansible.el
@@ -1,6 +1,6 @@
 ;;; module-ansible.el --- ansible integration
 
-(use-package ansible-mode
+(use-package ansible
   ; repo: https://github.com/spotify/ansiblefile-mode
   :mode (("\\.ansiblefile" . ansiblefile-mode)
          ("Ansiblefile\\'" . ansiblefile-mode)))
