210c9437d2871001388ecf0aecb51585fbcf0e2a
JesusMtnez <jesusmartinez93@gmail.com>
2018-05-15 23:40:36 +0200

    Improve scala module

--- modules/module-scala.el
+++ modules/module-scala.el
@@ -5,18 +5,18 @@
   :mode "\\.s\\(cala\\|bt\\)$"
   :config
   (setq scala-indent:align-forms t
-            scala-indent:align-parameters t
-            scala-indent:default-run-on-strategy scala-indent:operator-strategy))
+        scala-indent:align-parameters t
+        scala-indent:indent-value-expression t
+        scala-indent:default-run-on-strategy
+        scala-indent:operator-strategy))
 
-(use-package sbt-mode)
+(use-package sbt-mode
+  :commands sbt-start sbt-command)
 
 ;; ENSIME
 (use-package ensime
-  :commands (ensime ensime-mode ensime-scala-mode-hook)
-  :config
-  (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-  (add-hook 'scala-mode-hook #'ensime-mode)
-  (add-hook 'ensime-mode-hook #'eldoc-mode))
+  :pin "melpa-stable"
+  :commands (ensime ensime-mode ensime-scala-mode-hook))
 
 (provide 'module-scala)
 ;;; module-scala.el ends here
