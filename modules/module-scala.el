9553fed71e9a3a420879a754320e0b6ecd15f7e9
JesusMtnez <jesusmartinez93@gmail.com>
2018-06-05 16:00:48 +0200

    Enable eldoc-mode for Scala
    This function enables eldoc-mode to use ensime to indicate the type
    under the cursor, if ensime is running.

--- modules/module-scala.el
+++ modules/module-scala.el
@@ -1,5 +1,13 @@
 ;;; module-scala.el --- scala integration
 
+(defun scala/enable-eldoc ()
+  ;; Configure eldoc-mode for scala.
+  (setq-local eldoc-documentation-function
+              (lambda ()
+                (when (ensime-connected-p)
+                  (ensime-type-at-point))))
+  (eldoc-mode +1))
+
 (use-package scala-mode
   ; repo: https://github.com/ensime/emacs-scala-mode
   :mode "\\.s\\(cala\\|bt\\)$"
@@ -19,6 +27,8 @@
   :pin "melpa-stable"
   :commands (ensime ensime-mode ensime-scala-mode-hook)
   :hook (scala-mode . ensime-mode)
+  :init
+  (add-hook 'ensime-mode-hook 'scala/enable-eldoc)
   :config
   (add-hook 'ensime-mode-hook #'eldoc-mode)
   (setq ensime-startup-notification nil
