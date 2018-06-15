eba9d2d28d0f7948402597907784d4bacb095314
JesusMtnez <jesusmartinez93@gmail.com>
2018-06-15 15:43:44 +0200

    Use git-gutter to indicate git status in prog-mode
    Since git-gutter-mode works very well with new
    display-line-numbers-mode in emacs 26.1, I've updated just used
    git-gutter in prog-mode to show git status

--- modules/module-git.el
+++ modules/module-git.el
@@ -2,28 +2,12 @@
 
 (use-package git-gutter
   ; repo: https://github.com/syohex/emacs-git-gutter
-  :demand
-  :if (not window-system) ; Only in terminal
-  :config (progn (add-hook 'after-init-hook 'global-git-gutter-mode)
-                 (git-gutter:linum-setup)
-                 (setq git-gutter:hide-gutter t))
-  :diminish (git-gutter-mode . "gg"))
-
-(use-package git-gutter-fringe+
-  ; repo: https://github.com/nonsequitur/git-gutter-fringe-plus
-  :ensure t
-  :if window-system ; Only when window-system
-  :config (progn (add-hook 'after-init-hook 'global-git-gutter+-mode)
-                 (define-key git-gutter+-mode-map (kbd "C-x n") 'git-gutter+-next-hunk)
-                 (define-key git-gutter+-mode-map (kbd "C-x p") 'git-gutter+-previous-hunk)
-                 (define-key git-gutter+-mode-map (kbd "C-x v =") 'git-gutter+-show-hunk)
-                 (define-key git-gutter+-mode-map (kbd "C-x r") 'git-gutter+-revert-hunks)
-                 (define-key git-gutter+-mode-map (kbd "C-x t") 'git-gutter+-stage-hunks)
-                 (define-key git-gutter+-mode-map (kbd "C-x c") 'git-gutter+-commit)
-                 (define-key git-gutter+-mode-map (kbd "C-x C") 'git-gutter+-stage-and-define)
-                 (define-key git-gutter+-mode-map (kbd "C-x C-y") 'git-gutter+-stage-and-commit-whole-buffer)
-                 (define-key git-gutter+-mode-map (kbd "C-x U") 'git-gutter+-unstage-whole-buffer))
-  :diminish (git-gutter+-mode . "gg+"))
+  :unless (version< emacs-version "26.1")
+  :init
+  (progn (add-hook 'prog-mode-hook 'git-gutter-mode))
+  :config
+  (setq git-gutter:hide-gutter t)
+  :diminish git-gutter-mode)
 
 (use-package magit
   ; repo: https://github.com/magit/magit
