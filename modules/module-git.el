cc689f7ae96dddd1ff56c06dd520f6066cec24fb
JesusMtnez <jesusmartinez93@gmail.com>
2018-06-23 17:34:19 +0200

    Improve git-gutter behaviour

--- modules/module-git.el
+++ modules/module-git.el
@@ -1,15 +1,19 @@
 ;;; module-git.el --- git integration
+;; Repositories:
+;;  - `git-gutter`: https://github.com/syohex/emacs-git-gutter
+;;  - `magit`: https://github.com/magit/magit
 
 (use-package git-gutter
-  ; repo: https://github.com/syohex/emacs-git-gutter
   :unless (version< emacs-version "26.1")
-  :hook prog-mode
+  :hook ((prog-mode text-mode) . git-gutter-mode)
   :config
-  (setq git-gutter:hide-gutter t)
+  (setq git-gutter:hide-gutter t
+        git-gutter:update-interval 0.1
+        git-gutter:verbosity 0)
+  ;(add-to-list 'git-gutter:update-hooks 'focus-in-hook)
   :diminish git-gutter-mode)
 
 (use-package magit
-  ; repo: https://github.com/magit/magit
   :init (progn (global-git-commit-mode t))
   :config (progn (setq magit-last-seen-setup-instructions "1.4.0")))
 
