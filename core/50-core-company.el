0d59458761fb1bcfba0625d6ad714515cd0127f1
JesusMtnez <jesusmartinez93@gmail.com>
2018-07-11 18:06:58 +0200

    Refactor company completion config

--- core/50-core-company.el
+++ core/50-core-company.el
@@ -1,14 +1,14 @@
 ;;; 50-core-company.el --- company integration
 ;; TODO Company integration with helm: https://github.com/yasuyk/helm-company
 
-;; Company
 (use-package company
-  ; repo: https://github.com/company-mode/company-mode
   :demand
-  :config (progn (add-hook 'after-init-hook 'global-company-mode)
-                 (setq company-idle-delay 0.1)          ; Reduce company auto complete start time
-                 (setq company-minimum-prefix-length 1) ; Reduce prefix to start completion
-                 (setq company-show-numbers t))         ; Show number to quick access with M-number
+  :hook (after-init . global-company-mode)
+  :config
+  (progn
+    (setq company-idle-delay 0.1
+          company-minimum-prefix-length 1
+          company-show-numbers t))
   :diminish (company-mode . "CM"))
 
 (provide '50-core-company)
