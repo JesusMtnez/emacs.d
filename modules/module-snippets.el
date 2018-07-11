359ff41067a1984f083d1013784de3ed46b79941
JesusMtnez <jesusmartinez93@gmail.com>
2018-07-11 18:13:06 +0200

    Refactor snippets integration

--- modules/module-snippets.el
+++ modules/module-snippets.el
@@ -1,14 +1,16 @@
 ;;; module-snippets.el --- snippets integration
 
 (use-package yasnippet
+  :after company
   :hook (prog-mode . yas-minor-mode)
   :bind (:map yas-minor-mode-map
+              ("<tab>" . nil)
+              ("TAB" . nil)
               ("C-<tab>" . yas-expand))
-  :diminish
   :config
   (progn
-    (yas-reload-all)
     (setq yas-snippet-dirs (list (concat emacs-dir "snippets")))
+    (yas-reload-all)
     (setq yas-verbosity 1)))
 
 (provide 'module-snippets)
