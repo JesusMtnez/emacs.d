b3336e7e78fd85f3d3076f59ad251a40d4a49686
JesusMtnez <jesusmartinez93@gmail.com>
2019-04-02 08:04:47 +0200

    Add initial configuration for lsp-ui

--- core/70-core-lsp.el
+++ core/70-core-lsp.el
@@ -1,5 +1,13 @@
 ;;; 70-core-lsp.el --- lsp support
 
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (setq lsp-ui-doc-enable t
+        lsp-ui-doc-include-signture t
+        lsp-ui-doc-position 'at-point
+        lsp-ui-sideline-enable nil))
+
 (use-package lsp-mode
   :config
   (setq lsp-session-file (concat cache-dir ".lsp-session-v1")))
