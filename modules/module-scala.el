9ac7d3c89cd7d6734b7f9cd451cae3f015728097
JesusMtnez <jesusmartinez93@gmail.com>
2019-04-18 13:30:01 +0200

    Clean modules-scala

--- modules/module-scala.el
+++ modules/module-scala.el
@@ -1,23 +1,5 @@
 ;;; module-scala.el --- scala integration
 
-(defun scala/enable-eldoc ()
-  ;; Configure eldoc-mode for scala.
-  (setq-local eldoc-documentation-function
-              (lambda ()
-                (when (ensime-connected-p)
-                  (ensime-type-at-point))))
-  (eldoc-mode +1))
-
-(defun scala/completing-dot ()
-  "Insert a period and show company completions."
-  (interactive "*")
-  (when (s-matches? (rx (+ (not space)))
-                    (buffer-substring (line-beginning-position) (point)))
-    (delete-horizontal-space t))
-  (company-abort)
-  (insert ".")
-  (company-complete))
-
 (use-package scala-mode
   :mode "\\.s\\(cala\\|bt\\)$"
   :config
