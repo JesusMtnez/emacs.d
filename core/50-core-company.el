1351681533f5e24ea6c20e2f2119674435c2bbe9
JesusMtnez <jesusmartinez93@gmail.com>
2019-04-18 17:54:09 +0200

    Add company-quickhelp

--- core/50-core-company.el
+++ core/50-core-company.el
@@ -10,6 +10,12 @@
   (company-dabbrev-downcase nil)
   (company-dabbrev-ignore-case t))
 
+(use-package company-quickhelp
+  :after (company)
+  :custom
+  (company-quickhelp-delay 0.2)
+  (company-quickhelp-mode t))
+
 (use-package helm-company
   :after (company helm)
   :bind (:map company-active-map
