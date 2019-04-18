5b310d608713d43be652fef7e8e34e42cee49bfa
JesusMtnez <jesusmartinez93@gmail.com>
2019-04-18 11:59:26 +0200

    Add keybinds for {down/up}case-dwin

--- modules/module-text.el
+++ modules/module-text.el
@@ -4,5 +4,8 @@
   :bind (("M-p" . move-text-up)
          ("M-n" . move-text-down)))
 
+(global-set-key (kbd "M-u") 'upcase-dwim)
+(global-set-key (kbd "M-l") 'downcase-dwim)
+
 (provide 'module-text)
 ;;; module-text.el ends here
