2c58ffae87f28a520a5b3407fd68d5ed51243aa2
JesusMtnez <jesusmartinez93@gmail.com>
2019-06-26 09:09:04 +0200

    Move auto-package-update file to cache

--- core/10-core-packages.el
+++ core/10-core-packages.el
@@ -5,6 +5,7 @@
 
 (use-package auto-package-update
   :custom
+  (auto-package-update-last-update-day-path (concat cache-dir ".last-package-update-day"))
   (auto-package-update-delete-old-versions t))
 
 (provide '10-core-packages)
