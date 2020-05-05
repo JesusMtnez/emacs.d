25d6dc9d5d9c41f5262e721f9770edbad2baf165
JesusMtnez <jesusmartinez93@gmail.com>
2020-05-05 16:46:12 +0200

    Add nix-mode

--- core/20-core-ui.el
+++ core/20-core-ui.el
@@ -9,7 +9,7 @@
   (doom-themes-enable-bold t)
   (doom-themes-enable-italic t)
   :config
-  (load-theme 'doom-one-light t))
+  (load-theme 'doom-one t))
 
 (use-package dashboard
   :demand
---
+++ modules/module-nix.el
@@ -0,0 +1,7 @@
+;;; module-nix.el start here --- Nix integration
+
+(use-package nix-mode
+  :mode "\\.nix\\'")
+
+(provide 'module-nix)
+;;; module-nix.el ends here
